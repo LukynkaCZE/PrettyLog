e22d54f53b8e6a5591e7ad37455966d4257f3888
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-04-25 04:04:08 -0700

    :building_construction: Port to kt/n and kt/jvm, pt1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,43 @@
+@file:OptIn(ExperimentalKotlinGradlePluginApi::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
     `maven-publish`
-    kotlin("jvm") version "1.9.22"
-    application
+    kotlin("multiplatform") version "2.1.20"
 }
 
 group = "cz.lukynka"
-version = "1.4"
+version = "1.5" // bumped vers just in case since there are major internal changes.
 
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
-
 }
 
-application {
-    mainClass.set("MainKt")
-}
+kotlin {
+    // note: there doesnt seem to be an easy way to declare a main class/main entrypoint for natives other than jvm, so that might be an issue...
+    mingwX64()
+    macosX64()
+    macosArm64()
+    linuxX64()
+    linuxArm64()
+    jvm()
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "17"
+    sourceSets {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+            implementation("com.squareup.okio:okio:3.10.2")
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(kotlin("test-common"))
+        }
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "17"
-    }
-}
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    jvmToolchain(17)
 }
 
 publishing {
@@ -47,7 +56,6 @@ publishing {
             groupId = "cz.lukynka"
             artifactId = "pretty-log"
             version = version
-            from(components["java"])
         }
     }
 }
